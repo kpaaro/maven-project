6bb32939da7c814d53ae5bd8c6817edfbbd2cf9c
kaur <kaur.paaro@gmail.com>
2018-08-17 02:19:17 +0300

    dd

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -9,16 +9,15 @@ public class Greeter {
    * This is a constructor.
    */
   public Greeter() {
-
-  }
+    
+   }
 
   /**
    * This is a greet.
    *@param someone string rep for the greeted
-   *@return String
+   *@return greeting string
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
-  
 }
